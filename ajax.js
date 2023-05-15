1a809f35a16f79d8232ac5684e6442bb520123b8
aech2411 <127998081+aech2411@users.noreply.github.com>
2023-05-15 12:32:44 -0700

    commit

---
+++ ajax.js
@@ -0,0 +1,36 @@
+function getXMLHttpRequest() {
+    try {
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch(e) {
+            return new ActiveXObject("Msxml2.XMLHTTP");
+        }
+    }
+    catch(e) {
+        return new XMLHttpRequest();
+    }
+}
+
+function callAjax() {
+    var url = "clock.php";
+    var myRandom = parseInt(Math.random()*99999999);
+    var myRequest = getXMLHttpRequest(); // move this line up
+    myRequest.open("GET", url + "?rand=" + myRandom, true);
+    myRequest.onreadystatechange = responseAjax;
+    myRequest.send(null);
+}
+
+function responseAjax() {
+    var myRequest = this; // add this line
+    if(myRequest.readyState == 4) {
+        if(myRequest.status == 200) {
+        var now = new Date();
+        var localTime = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        var serverTime = myRequest.responseText;
+        document.getElementById("clock").innerHTML = "Server: " + serverTime + "<br /> Local: " + localTime;
+        }else {
+            alert("An error has occurred:" + myRequest.statusText);
+        }
+    }
+}
