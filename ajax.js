46410ca46c9ec5faf3a1214ebacd9311ea179e57
aech2411 <127998081+aech2411@users.noreply.github.com>
2023-05-16 15:19:30 -0700

    commit

--- ajax.js
+++ ajax.js
@@ -1,36 +1,32 @@
-function getXMLHttpRequest() {
-    try {
-        try {
+function getXMLHttpRequest(){
+    try{
+        try{
             return new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        catch(e) {
+        }catch(e){
             return new ActiveXObject("Msxml2.XMLHTTP");
-        }
-    }
-    catch(e) {
+        } 
+    }catch(e){
         return new XMLHttpRequest();
-    }
+    } 
 }
-
-var myRequest = getXMLHttpRequest();
-
-function callAjax() {
+function callAjax(){
     var url = "clock.php";
     var myRandom = parseInt(Math.random()*99999999);
-    myRequest.open("GET", url + "?rand=" + myRandom, true);
+    myRequest.open("GET",url+"?rand="+myRandom,true);
     myRequest.onreadystatechange = responseAjax;
     myRequest.send(null);
 }
-
-function responseAjax() {
-    if(myRequest.readyState == 4) {
-        if(myRequest.status == 200) {
-        var now = new Date();
-        var localTime = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-        var serverTime = myRequest.responseText;
-        document.getElementById("clock").innerHTML = "Server: " + serverTime + "<br /> Local: " + localTime;
-        }else {
-            alert("An error has occurred:" + myRequest.statusText);
+function responseAjax(){
+    if (myRequest.readyState == 4){
+        if(myRequest.status == 200){
+            var now = new Date();
+            var localTime = now.getHours() + ";"+ now.getMinutes() + ":" + now.getSeconds();
+            var serverTime = myRequest.responseText;
+            document.getElementById("clock").innerHTML = "Server: " + serverTime + "<br />Local: " +localTime;
+        }
+        else{
+            alert("An error has occurred: " + myRequest.statusText);
         }
     }
 }
+var myRequest = getXMLHttpRequest();
